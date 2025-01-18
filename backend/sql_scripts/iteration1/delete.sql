20753c55c03dc09f53cbe83de9152e41b5765d3f
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2025-01-18 18:46:19 +0300

    Денормализация
    Переход от 2НФ к 1НФ для упрощения организации сущностей

---
+++ backend/sql_scripts/iteration1/delete.sql
@@ -0,0 +1,41 @@
+-- Удаление всех данных из таблиц
+
+delete from "Служба_поддержки";
+
+delete from "Расписание_консультантов";
+
+delete from "Консультант";
+
+delete from "Клиент";
+
+delete from "Заказ";
+
+delete from "Сообщение";
+
+delete from "Тип_продукции";
+
+delete from "Продукция_в_заказе";
+
+delete from "Завод";
+
+delete from "Склад_готовой_продукции";
+
+delete from "Готовая_продукция";
+
+delete from "Склад_сырья";
+
+delete from "Материалы";
+
+delete from "Тип_материала";
+
+delete from "Организация";
+
+delete from "Сотрудничество";
+
+delete from "Цех";
+
+delete from "Расписание_сотрудников";
+
+delete from "Сотрудник";
+
+delete from "Оборудование";
